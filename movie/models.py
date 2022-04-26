03246edf9abe0f4f3fa8664ae85ffec6623c8cc9
Lawrence Bonilla <60842489+Lawrrr@users.noreply.github.com>
2022-04-26 23:11:15 +0800

    Get all movies list (#2)

--- movie/models.py
+++ movie/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Genre(models.Model):
     genre = models.CharField(max_length=200, null=False)
     created_on = models.DateTimeField(auto_now_add=True)
@@ -11,6 +12,7 @@ class Genre(models.Model):
     class Meta:
         ordering = ['genre']
 
+
 class Director(models.Model):
     first_name = models.CharField(max_length=30, null=False)
     last_name = models.CharField(max_length=30, null=False)
@@ -21,17 +23,18 @@ class Director(models.Model):
     def __str__(self):
         return (self.first_name + " " + self.last_name)
 
+
 class Movie(models.Model):
     title = models.CharField(max_length=200, null=False)
     genre = models.ManyToManyField(Genre)
-    director = models.ForeignKey(Director, on_delete=models.CASCADE, null=False)
+    director = models.ForeignKey(
+        Director, on_delete=models.CASCADE, null=False)
     release_date = models.DateField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
-    
+
     class Meta:
         ordering = ['-created_on']
-
