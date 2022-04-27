fe69ffcf583f22e49620973ae34e53e65fbeb024
LawrViseo <104424599+LawrViseo@users.noreply.github.com>
2022-04-27 15:01:14 +0800

    Create new movie API (#4)

--- movie/urls.py
+++ movie/urls.py
@@ -4,5 +4,6 @@ from .views import movie
 
 urlpatterns = [
     path('movies', movie.get_all),
-    path('movie/<int:id>', movie.get)
+    path('movie/<int:id>', movie.get),
+    path('movie/create', movie.create)
 ]
--- movie/views/movie.py
+++ movie/views/movie.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from ..models import Movie
+from ..models import Movie, Director
 from ..serializers import MovieSerializer
 
 
@@ -10,6 +10,7 @@ def get_all(request):
     if request.method == 'GET':
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
+
         return Response(serializer.data)
 
 @api_view(['GET'])
@@ -18,10 +19,56 @@ def get(request, **kwargs):
         try:
             movie = Movie.objects.get(id=kwargs['id'])
             serializer = MovieSerializer(movie)
+
             return Response(serializer.data)
         except:
             data = {
                 "status": "error",
                 "message": "Movie doesn't exist..."
             }
+
             return Response(data, status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['POST'])
+def create(request):
+    if request.method == 'POST':
+        movie = request.data
+        try:
+            checkMovie = Movie.objects.filter(title=movie['title']).exists()
+
+            if not checkMovie:
+                getDirector = Director.objects.get(id=movie['director'])
+
+                newMovie = Movie.objects.create(
+                    title=movie['title'],
+                    director=getDirector,
+                    release_date=movie['release_date']
+                )
+                
+                # Add genre/s to newly created movie
+                for genre in movie['genre']:
+                    newMovie.genre.add(genre)
+                    
+                movieSerialize = MovieSerializer(newMovie)
+                data = {
+                    "status": "success",
+                    "message": "New movie added!",
+                    "movie": movieSerialize.data
+                }
+
+                return Response(data)
+            else:
+                data = {
+                    "status": "error",
+                    "message": "Movie already in the database."
+                }
+
+                return Response(data, status=status.HTTP_409_CONFLICT)
+
+        except:
+            data = {
+                "status": "error",
+                "message": "Movie not created."
+            }
+
+            return Response(data, status=status.HTTP_400_BAD_REQUEST)
