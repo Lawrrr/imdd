0f20900ab8cc7ed735dc15339a2425ecac1a6434
Lawrence Bonilla <lawrence.bonilla@viseo.com>
2022-04-28 16:08:10 +0800

    Update movie details API

--- movie/urls.py
+++ movie/urls.py
@@ -4,7 +4,8 @@ from .views import movie
 
 urlpatterns = [
     path('movies', movie.get_all),
+    path('movie/create', movie.create),
     path('movie/<int:id>', movie.get),
-    path('movie/<int:id>/delete', movie.delete),
-    path('movie/create', movie.create)
+    path('movie/<int:id>/update', movie.update),
+    path('movie/<int:id>/delete', movie.delete)
 ]
