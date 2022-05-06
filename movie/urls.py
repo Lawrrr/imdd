d8c46594365ae12b57ecbca69ea75bb4954ce5ce
LawrViseo <104424599+LawrViseo@users.noreply.github.com>
2022-05-06 13:53:08 +0800

    Develop directors CRUD (#12)
    * Director CRUD
    
    * Added update and delete
    
    Co-authored-by: Lawr <lawrboni@gmail.com>

--- movie/urls.py
+++ movie/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
+from rest_framework import routers
 
 from .views import movie, director
 
-urlpatterns = [
-    path('movies', movie.get_all),
+router = routers.SimpleRouter()
+router.register(r'directors', director.DirectorViewset, basename='directors')
+
+urls = [
+    path('movies/', movie.get_all),
     path('movie/create', movie.create),
     path('movie/<int:id>', movie.get),
     path('movie/<int:id>/update', movie.update),
-    path('movie/<int:id>/delete', movie.delete),
-
-    path('directors', director.get_all)
+    path('movie/<int:id>/delete', movie.delete)
 ]
+
+# Urls for Viewset
+urlpatterns = urls + router.urls
--- movie/views/director.py
+++ movie/views/director.py
@@ -1,13 +1,75 @@
-from rest_framework.decorators import api_view
+from rest_framework import viewsets, status
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 from ..models import Director
 from ..serializers import DirectorSerializer
 
 
-@api_view(['GET'])
-def get_all(request):
-    if request.method == 'GET':
+class DirectorViewset(viewsets.ViewSet):
+    """
+    CRUD Directors
+    """
+
+    def list(self, request):
         directors = Director.objects.all()
         serializer = DirectorSerializer(directors, many=True)
 
         return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        directors = Director.objects.all()
+        director = get_object_or_404(directors, pk=pk)
+        serializer = DirectorSerializer(director)
+
+        return Response(serializer.data)
+
+    def create(self, request):
+        inputData = request.data
+
+        director = Director.objects.create(
+            first_name=inputData['first_name'],
+            last_name=inputData['last_name'],
+            date_of_birth=inputData['date_of_birth']
+        )
+        serializer = DirectorSerializer(director)
+
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        try:
+            updateData = request.data
+            directors = Director.objects.all()
+            director = get_object_or_404(directors, pk=pk)
+
+            for key, value in updateData.items():
+                setattr(director, key, value)
+
+            director.save()
+            serializer = DirectorSerializer(director)
+
+            return Response(serializer.data)
+        except:
+            data = {
+                'status': 'error',
+                'message': 'Update unsuccessful.'
+            }
+        
+        return Response(data, status=status.HTTP_404_NOT_FOUND)
+
+    def destroy(self, request, pk=None):
+        try:
+            Director.objects.get(id=pk).delete()
+
+            data = {
+                'status': 'success',
+                'message': 'Director successfully deleted.'
+            }
+
+            return Response(data)
+        except:
+            data = {
+                'status': 'error',
+                'message': 'Deletion unsuccessful'
+            }
+
+            return Response(data, status=status.HTTP_404_NOT_FOUND)
