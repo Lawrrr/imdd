bff35a2437198f59078cbac99c5b236760dce1ab
Lawrence Bonilla <lawrence.bonilla@viseo.com>
2022-05-26 13:29:02 +0800

    Update Director Unit Test

--- movie/tests/test_director.py
+++ movie/tests/test_director.py
@@ -6,9 +6,9 @@ from ..models import Director
 
 class DirectorTestCase(TestCase):
     def setUp(self):
-        Director.objects.create(first_name='Johnny', last_name='Test', date_of_birth='1920-01-01')
-        Director.objects.create(first_name='Bruce', last_name='Wayne', date_of_birth='1989-11-11')
-        Director.objects.create(first_name='Guy', last_name='Gardner', date_of_birth='2000-06-21')
+        self.director1 = Director.objects.create(first_name='Johnny', last_name='Test', date_of_birth='1920-01-01')
+        self.director2 = Director.objects.create(first_name='Bruce', last_name='Wayne', date_of_birth='1989-11-11')
+        self.director3 = Director.objects.create(first_name='Guy', last_name='Gardner', date_of_birth='2000-06-21')
 
     def test_can_list_all_directors(self):
         directors = Director.objects.all()
@@ -18,9 +18,9 @@ class DirectorTestCase(TestCase):
     def test_can_retrieve_director(self):
         director = Director.objects.get(id=1)
 
-        self.assertEqual(director.id, 1)
-        self.assertEqual(director.first_name, 'Johnny')
-        self.assertEqual(director.last_name, 'Test')
+        self.assertEqual(director.id, self.director1.id)
+        self.assertEqual(director.first_name, self.director1.first_name)
+        self.assertEqual(director.last_name, self.director1.last_name)
         self.assertEqual(director.date_of_birth, date(1920, 1, 1))
 
     def test_can_create_director(self):
@@ -32,7 +32,7 @@ class DirectorTestCase(TestCase):
 
     def test_can_update_director(self):
         directors = Director.objects.all()
-        director = get_object_or_404(directors, pk=1)
+        director = get_object_or_404(directors, pk=self.director2.id)
 
         updateDirector = {
             'first_name': 'new',
@@ -44,10 +44,9 @@ class DirectorTestCase(TestCase):
             setattr(director, key, value)
 
         director.save()
-
-        self.assertEqual(director.first_name, 'new')
-        self.assertEqual(director.last_name, 'name')
-        self.assertEqual(director.date_of_birth, '1999-04-21')
+        self.assertEqual(director.first_name, updateDirector.get('first_name'))
+        self.assertEqual(director.last_name, updateDirector.get('last_name'))
+        self.assertEqual(director.date_of_birth, updateDirector.get('date_of_birth'))
 
     def test_can_delete_director(self):
         Director.objects.get(id=1).delete()
